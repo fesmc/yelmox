23ede8245784096b444847b4d26839614b162f08
Alexander Robinson <alexander.robinson@awi.de>
2026-07-10 23:07:11 +0200

    cleanup: remove pre-configme/pre-runme tooling
    Superseded by configme (install/config) and runme (submit/ensemble),
    none referenced by the current docs:
    - install.py (use `configme install`)
    - submit.sh, submit_test.sh, yelmo_ensemble.py (use runme)
    - check_sim.{f90,jl,sh} legacy PD-ensemble diagnostic (+ its Makefile target)
    
    config.py kept for now as the legacy install fallback.

--- submit_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-#SBATCH -p long
-#SBATCH -J yelmo 
-#SBATCH -o yelmo.out
-#SBATCH -e yelmo.err
-#SBATCH --mem=1800
-#SBATCH -t 7-02:30:15
-
-runme -r -e yelmox -o output/deglaciation_ant_quad -n par/yelmo_Antarctica_deglaciation_quad-nl-slope.nml
